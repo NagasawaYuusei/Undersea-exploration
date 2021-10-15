1b93ff28f2fbedeb3c5f1a639e1f6650f5d378a0
NagasawaYusei <curga3013@gmail.com>
2021-10-15 18:15:18 +0900

    スクリプト途中

---
+++ Assets/Script/RemainsManeger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] m_mass;
+
+    public void SetBoard()
+    {
+
+    }
+
+    public void GetRemain()
+    {
+
+    }
+}
