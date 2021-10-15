fb54919055d936fe5269da9e5c31603f134ec5e3
NagasawaYusei <curga3013@gmail.com>
2021-10-15 19:42:21 +0900

    Air完成

--- Assets/Script/ShipManeger.cs
+++ Assets/Script/ShipManeger.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class ShipManeger : MonoBehaviour
 {
-
-    void Update()
+    [SerializeField] GameObject[] m_kuukis;
+    [SerializeField] GameObject[] m_senn;
+    [SerializeField] PlayerManeger m_playerManeger;
+    int m_get;
+    public void NowShip()
     {
-        
+        for(int i = 0; i < m_get; i++)
+        {
+            m_kuukis[i].SetActive(false);
+            m_senn[i].SetActive(false);
+        }
     }
 }
