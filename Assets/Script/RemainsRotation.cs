2e3d5ffab186d47a2a965051b929e31c972811dd
伊東聖矢 <tvedaamamesab@gmail.com>
2021-10-20 20:31:16 +0900

    完成

---
+++ Assets/Script/RemainsRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemainsRotation : MonoBehaviour
+{
+    void Start()
+    {
+        float randomRotation = Random.Range(0, 360);
+        //this.transform.rotation = Random.rotation;
+        this.transform.Rotate(0, 0, randomRotation);
+    }
+}
