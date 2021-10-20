ef84b712d8a4b345e7a71f1ec5443b7297a69a2c
NagasawaYusei <curga3013@gmail.com>
2021-10-20 20:22:29 +0900

    Revert "Revert "Revert "スクリプト途中"""
    This reverts commit 0ad870893719cd27e2cd4e8b1deb090227d224df.

--- Assets/Script/Dice.cs
+++ Assets/Script/Dice.cs
@@ -5,25 +5,18 @@ using UnityEngine;
 public class Dice : MonoBehaviour
 {
     /// <summary>サイコロの出目の和</summary>
-    public int DiceSum { get { return m_diceSum; } }
-    public GameObject Dice1 { get { return m_dice1; } }
-    public GameObject Dice2 { get { return m_dice2; } }
+    public int DiceSum { get { return m_diceSum; } } 
 
     [SerializeField] GameObject[] m_diceObjects;
     [SerializeField] Transform[] m_diceTransforms;
-    [SerializeField] GameObject m_dicePanel;
-    [SerializeField] float m_diceDisplayTime;
     List<GameObject> m_dice = new List<GameObject>();
     private int m_randomDiceValue1;
     private int m_randomDiceValue2;
     private int m_diceSum;
-
-    GameObject m_dice1;
-    GameObject m_dice2;
-    
     void Start()
     {
         SetUp();
+        RandomDice();
     }
 
     void SetUp()
@@ -41,22 +34,9 @@ public class Dice : MonoBehaviour
         var dice1 = Instantiate(m_dice[m_randomDiceValue1]);
         var dice2 = Instantiate(m_dice[m_randomDiceValue2]);
 
-        m_dice1 = dice1;
-        m_dice2 = dice2;
-
         dice1.transform.position = m_diceTransforms[0].position;
         dice2.transform.position = m_diceTransforms[1].position;
-    }
 
-    public void Stop()
-    {
-        RandomDice();
-        StartCoroutine(Move());
-    }
 
-    IEnumerator Move()
-    {
-        yield return new WaitForSeconds(m_diceDisplayTime);
-        m_dicePanel.SetActive(false);
     }
 }
