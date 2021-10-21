877bcfe6c8d6f150dff7259c412c575aa90eb448
伊東聖矢 <tvedaamamesab@gmail.com>
2021-10-21 18:59:02 +0900

    結果発表のUIを作成した

---
+++ Assets/Script/ResultManeger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ResultManeger : MonoBehaviour
+{
+    [SerializeField] int m_playerNum;
+    [SerializeField] string[] m_playerName;
+    [SerializeField] GameObject m_player1Sankaku;
+    [SerializeField] GameObject m_plyaer1Sikaku;
+    [SerializeField] GameObject m_plyaer1Gokaku;
+    [SerializeField] GameObject m_plyaer1Rokukaku;
+
+    [SerializeField] TextMeshProUGUI m_player1ResultText; 
+    public void Result()
+    {
+        
+    }
+}
